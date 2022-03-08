378f0a02b071ee894f5f090418f57d054ce8393e
Ulinnuha <ulinnuha@dr.com>
2022-03-08 19:33:27 +0700

    tambah detail dikit di function

--- pertemuan04/function.php
+++ pertemuan04/function.php
@@ -1,6 +1,6 @@
 <?php
 
-function salam($waktu = "mornin'", $nama = "you there") {
+function salam($waktu = "mornin'", $nama = "you there") { // "mornin" dan "you there" = parameter default, in case di body tidak diinput parameternya
     return "good $waktu, $nama!";
 }
 
